4af7bf5af728555951a343d3e8cdddae090904a0
Pierluigi Chiodin Omnys <pierluigi.chiodin@omnys.com>
2023-10-16 18:09:12 +0200

    avanti

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  getOrder: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]    // restituisco un array di due elementi composto dall'iesimo elemento di starterMenu e l'iesimo elemento di mainMenu
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,56 @@ const restaurant = {
     },
   },
 };
+
+/*
+const array = [1, 2, 3, 4, 5];
+const [a, b, c] = array;
+
+console.log(a, b, c);
+
+let [primo, , secondo] = restaurant.categories;
+console.log(primo, secondo);
+
+// per scambiare il primo col secondo senza usare una variabile di comodo in cui parcheggiare un valore: 
+// in pratica creo un array con le due variabili estratte invertendone l'ordine e lo assegno nuovamente alle stesse variabili usando nuovamente il destructuring
+[primo, secondo] = [secondo, primo];
+
+console.log(primo, secondo);
+*/
+/*
+let a = "ciao";
+let b = "pippo";
+console.log(a, b);
+
+//scambio usando destructuring 
+[a, b] = [b, a];
+console.log(a, b);
+
+//chiamo getOrder e uso destructuring per valorizzare due variabili con il risultato, che è un array di due elementi
+const [starter, menu] = restaurant.getOrder(1, 2);
+console.log(starter, menu);
+
+function prova() {
+  return ["ciao", "da", "giggi"];
+}
+
+const [ciao, da, giggi] = prova();
+console.log(ciao, da, giggi);
+
+// posso fare anche il destructuring nel destructuring, se l'array in input ha un sottoarray
+const listaNum = [1, 2, [3, 4]];
+const [uno, , [tre, quattro]] = listaNum;
+console.log(uno, tre, quattro);   // SALTO IL DUE E ESTRAGGO TRE E QUATTRO CHE STANNO IN UN ARRAY NELL'ARRAY
+*/
+
+const { name, categories, mainMenu } = restaurant;
+console.log(name, categories, mainMenu);
+
+const mioObj = {
+  propA: "ciao",
+  propB: "da",
+  propC: "giggi"
+}
+
+const { propA, propB, propC } = mioObj;
+console.log(propA, propB, propC);
