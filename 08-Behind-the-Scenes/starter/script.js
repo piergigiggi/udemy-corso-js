3377244c5a831cd1e1d76d6e0853d49290022182
Pierluigi Chiodin <piergiggisc@gmail.com>
2023-10-16 07:41:33 +0200

    avanti

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  {
+    function test() {
+      console.log('ciao da', firstName);
+    }
+    test();
+  }
+  //   test();
+
+  printAge();
+  return age;
+}
+
+var firstName = 'giggi';
+calcAge(1994);
